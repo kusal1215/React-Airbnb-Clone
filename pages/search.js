3044347fd480e3ae50f65fb1798c5899670a9e92
IT18119572_ChathurangaWWPK <it18119572@my.sliit.lk>
2021-08-27 18:07:36 +0530

    day 2 of airbnb clone

---
+++ pages/search.js
@@ -0,0 +1,73 @@
+import Header from "../components/Header";
+import Footer from '../components/Footer';
+import { useRouter } from "next/dist/client/router";
+import {format} from "date-fns";
+import InfoCard from "../components/InfoCard";
+
+function Search({ searchResults }) {
+
+    const router = useRouter();
+
+    console.log(searchResults)
+
+    // ES6 Destructuring
+    const {location , startDate , endDate , noOfGuests} = router.query;
+
+    const formattedStartDate = format(new Date(startDate), "dd MMM yy")
+    const formattedEndDate = format(new Date(endDate), "dd MMM yy")
+    const range = `${formattedStartDate} - ${formattedEndDate}`
+
+    return (
+        <div>
+            <Header placeholder={`${location} | ${range} | ${noOfGuests} guests` } />
+
+            <main className="flex">
+
+                <section className="flex-grow pt-14 px-6">
+                    <p className="text-xs">300+ stays - {range} - for {noOfGuests} guests</p>
+
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+
+                    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                        <p className="filter-btn">Cancelation Flexibility</p>
+                        <p className="filter-btn">Type of Place </p>
+                        <p className="filter-btn">Rooms and Beds </p>
+                        <p className="filter-btn">Price </p>
+                        <p className="filter-btn">More Filters</p>
+                    </div>
+
+                    <div className="flex flex-col">
+                        {searchResults.map(({ img, location, title, description, star, price, total }) => (
+                            <InfoCard 
+                                key = {img}
+                                img = {img}
+                                location = {location}
+                                title = {title}
+                                description = {description}
+                                star = {star}
+                                price = {price}
+                                total = {total}
+                            />
+                        ))}
+                    </div>
+
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Search
+
+export async function getServerSideProps() {
+
+    const searchResults = await fetch("https://links.papareact.com/isz").then(res => res.json());
+
+    return {
+        props: {
+            searchResults,
+        }
+    }
+}
